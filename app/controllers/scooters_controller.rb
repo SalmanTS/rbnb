bf85c3a53d9bf5ebe309bc89cb88a821b8ea8f39
Babadi MUKANYA <babadi.mukanya@gmail.com>
2024-11-19 14:54:03 +0100

    first configs

--- app/controllers/scooters_controller.rb
+++ app/controllers/scooters_controller.rb
@@ -1,2 +1,22 @@
 class ScootersController < ApplicationController
+  def new
+    @scooter = Scooter.new
+  end
+
+  def create
+    @scooter = Scooter.new(scooter_params)
+
+    if @scooter.save
+      redirect_to scooters_path, notice: 'Scooter added successfully.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def scooter_params
+    params.require(:scooter).permit(:description, :price, :image)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,3 +14,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 end
+
+
+# config/routes.rb
+resources :scooters, only: [:new, :create]
